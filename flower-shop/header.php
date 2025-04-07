cf4cfe2139ea476986a6a839ecd5abc464dbb9b6
Dikoshi <137956014+Dikoshi@users.noreply.github.com>
2025-04-07 15:28:09 +0400

    flower-shop site

---
+++ flower-shop/header.php
@@ -0,0 +1,14 @@
+<header class="site-header">
+    <div class="header-container">
+        <a href="index.php" class="logo">🌸 Магазин</a>
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li><a href="index.php" class="nav-link">Главная</a></li>
+                <li><a href="products.php" class="nav-link">Каталог</a></li>
+                <li><a href="about.php" class="nav-link">О нас</a></li>
+                <li><a href="contacts.php" class="nav-link">Контакты</a></li>
+                <li><a href="cart.php" class="nav-link">🛒 Корзина</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
